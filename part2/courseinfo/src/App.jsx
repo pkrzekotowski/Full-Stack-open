4e25e315ce115f4cef9ab24e769631fcccad239b
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2024-08-18 15:57:49 +0200

    completed exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = ({ course }) => <h2>{course.name}</h2>
+
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Content = ({ course }) => {
+  return (
+    <div>
+    {course.parts.map(part =>
+        <Part key={part.id} exercises={part.exercises} name={part.name}/>
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

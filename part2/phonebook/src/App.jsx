16acd3858bcd37aaf3c66d0b02e768436898dec4
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2024-08-22 17:19:37 +0200

    completed exercise 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import phonebookService from './services/notes'
 
 const Persons = ({ person }) => <div>{person.name} {person.number}</div>
 
@@ -48,9 +48,9 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    phonebookService
+      .getAll()
+      .then(personList => setPersons(personList))
   }, [])
 
   const handlePersonChange = (e) => {
@@ -75,10 +75,10 @@ const App = () => {
         number: newPerson.number,
       }
 
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      phonebookService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewPerson({name: '', number: ''})
         })
 
